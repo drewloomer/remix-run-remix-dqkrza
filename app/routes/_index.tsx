3f4e4229c06b1ca82f6b85307aba95a7b9af1ba2
Drew Loomer <drew.loomer@curology.com>
2024-04-11 12:17:17 -0400

    Broken cloudflare example.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,26 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { json, useLoaderData } from '@remix-run/react';
+import { Markdown } from '~/components';
+import { getFileContentWithCache } from '~/services/github.server';
+import { parse } from '~/services/markdoc.server';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export async function loader({ context }: LoaderFunctionArgs) {
+	const content = await getFileContentWithCache(context, 'README.md');
+
+	return json(
+		{
+			content: parse(content),
+		},
+		{
+			headers: {
+				'Cache-Control': 'public, max-age=3600',
+			},
+		},
+	);
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const { content } = useLoaderData<typeof loader>();
+
+	return <Markdown content={content} />;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
+import {
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-installGlobals();
+import { getLoadContext } from './load-context';
 
 export default defineConfig({
-  base: '/dashboard/',
-  plugins: [
-    remix({
-      basename: '/dashboard/',
-    }),
-    tsconfigPaths(),
-  ],
+	base: '/dashboard/',
+	plugins: [
+		remixCloudflareDevProxy({ getLoadContext }),
+		remix({
+			basename: '/dashboard/',
+			manifest: true,
+		}),
+		tsconfigPaths(),
+	],
 });
